8d25157ed40a5293c7d114ae927304ddf4742d2b
Chien <remwasaiko@gmail.com>
2022-09-19 14:19:16 +0700

    update .

--- khong_can_thiet/c.js
+++
@@ -1,8 +0,0 @@
-$(".hop-thoai").addClass('show');
-$(".background").addClass('show');
-
-var banner = setInterval(function(){
-$(".hop-thoai").removeClass('show');
-$(".background").removeClass('show');
-clearInterval(banner);
-},2000);
